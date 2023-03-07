7c1cf0792954a1e1bb65d652366e7d193a4241a0
Max Tauroza <maxtauroza@gmail.com>
2023-03-07 16:45:21 +0000

    Solved first 6 object-based problems

--- src/__tests__/objects.test.js
+++ src/__tests__/objects.test.js
@@ -43,7 +43,7 @@ describe('getName', () => {
 });
 
 describe('getProperty', () => {
-  xit('returns the given property', () => {
+  it('returns the given property', () => {
     expect(
       getProperty('age', {
         name: 'Fred',
@@ -70,7 +70,7 @@ describe('hasProperty', () => {
     age: 23
   };
 
-  xit('returns true if the object has the given property', () => {
+  it('returns true if the object has the given property', () => {
     expect(hasProperty('age', fred)).toBe(true);
     expect(hasProperty('name', tom)).toBe(true);
     expect(hasProperty('favouriteColour', fred)).toBe(false);
@@ -79,7 +79,7 @@ describe('hasProperty', () => {
 });
 
 describe('isOver65', () => {
-  xit('returns true if the person is aged over 65', () => {
+  it('returns true if the person is aged over 65', () => {
     const jim = {
       name: 'Jim',
       age: 66
@@ -102,7 +102,7 @@ describe('isOver65', () => {
 });
 
 describe('getAges', () => {
-  xit('returns the ages of each person in the array', () => {
+  it('returns the ages of each person in the array', () => {
     const jim = {
       name: 'Jim',
       age: 66
--- src/objects.js
+++ src/objects.js
@@ -11,19 +11,21 @@ const getName = object => {
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
+  return person.age > 65;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(personAge => {
+    return personAge.age;
+  });
 };
 
 const findByName = (name, people) => {
